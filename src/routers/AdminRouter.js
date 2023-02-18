4027621e1bd90d00d29b0c535af0b3bec7c0a0bb
Shital769 <110831382+Shital769@users.noreply.github.com>
2023-02-18 23:54:11 +1100

    admin registration done

--- src/routers/AdminRouter.js
+++ src/routers/AdminRouter.js
@@ -2,13 +2,21 @@ import express from "express";
 const router = express.Router();
 import { v4 as uuidv4 } from "uuid";
 import { createNewAdmin } from "../models/admin/AdminModel.js";
+import { newAccountEmailVerificationEmail } from "../utils/nodemailer.js";
 
 //admin registration
 router.post("/register", async (req, res, next) => {
   try {
-    // req.body.password = hashpassword(req.body.password)
+    req.body.password = hashpassword(req.body.password);
+
+    req.body.emailVerificationCode = uuidv4();
 
     const result = await createNewAdmin(req.body);
+
+    if (result?._id) {
+      const uniqueLink = `${process.env.FRONTEND_ROOT_URL}/verify?c=${result.emailVerificationCode}&email=${result.email}`;
+      newAccountEmailVerificationEmail(uniqueLink, result);
+    }
 
     res.json({
       status: "error",
@@ -23,4 +31,4 @@ router.post("/register", async (req, res, next) => {
   }
 });
 
-export default router;
+//admin user email verification
